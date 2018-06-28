a3cad4a361b1341eecf687ec02e7cc94833c4476
xweiba <lh1738289913@gmail.com>
2018-06-28 21:58:17 +0800

    添加 hql 测试

---
+++ 02-SpringBootWeb-Rest/src/test/java/ml/xiaoweiba/service/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package ml.xiaoweiba.service;
+
+import ml.xiaoweiba.entity.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void userJpa() {
+        userRepository.save(new User("AAA", 10));
+        userRepository.save(new User("BBB", 20));
+        userRepository.save(new User("CCC", 30));
+        userRepository.save(new User("DDD", 40));
+        userRepository.save(new User("EEE", 50));
+        userRepository.save(new User("FFF", 60));
+        userRepository.save(new User("GGG", 70));
+        userRepository.save(new User("HHH", 80));
+        userRepository.save(new User("III", 90));
+        userRepository.save(new User("JJJ", 100));
+
+        Assert.assertEquals(10, userRepository.count());
+    }
+
+    @Test
+    public void hqlTest() {
+        Assert.assertEquals(80, userRepository.findByName("HHH").getAge().intValue());
+    }
+}
