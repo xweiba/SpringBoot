9a191cd3ed317788e3ebbc187e63fc55313c30a6
xweiba <lh1738289913@gmail.com>
2018-06-28 22:00:07 +0800

    启动方法

---
+++ 02-SpringBootWeb-Thymeleaf/src/main/java/ml/xiaoweiba/Application.java
@@ -0,0 +1,17 @@
+package ml.xiaoweiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: demo
+ * @description: 启动
+ * @author: Mr.xweiba
+ * @create: 2018-06-26 17:28
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ 04-Spring-Data-Jap/src/main/java/ml/xiaoweiba/Application.java
@@ -0,0 +1,19 @@
+package ml.xiaoweiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @program: demo
+ * @description: 启动方法
+ * @author: Mr.xweiba
+ * @create: 2018-06-26 15:05
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
